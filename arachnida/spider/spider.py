af771d5abeb80ca15a6de4356b179734913123a9
Yassin MEHLIL <ymehlil@paul-f4Br5s5.clusters.42paris.fr>
2024-03-28 16:46:17 +0100

    feat(dl) download all images from URL (Only jpg/jpeg, gif, bmp and png)

--- arachnida/spider/spider.py
+++ arachnida/spider/spider.py
@@ -1,11 +1,34 @@
 import requests
 import argparse
+import os
 
-# This progran will get a webpage and download all the images 
 
-def get_page(url):
-	response = requests.get(url)
-	return response.text
+# This progran will get a webpage and download all the images it will only download the images in jpg/jpeg, png, gif, bmp format
+
+def get_images(url, path):
+	r = requests.get(url)
+	if r.status_code == 200: # Check if the request was successful
+		content = r.text # Get the content of the page
+		images = content.split("<img")
+		# Loop through the images
+		for i in images:
+			src = i.split("src=")[1].split(" ")[0].strip("\"") # Find the source of the image
+			if src.endswith((".jpg", ".jpeg", ".png", ".gif", ".bmp")):
+				r = requests.get(src)
+				if r.status_code == 200:
+					# Get the content of the image
+					content = r.content
+					# Get the name of the image
+					name = src.split("/")[-1]
+					# Save the image
+					with open(os.path.join(path, name), "wb") as f:
+						f.write(content)
+				else:
+					print("Error getting image: ", src)
+	else:
+		print("Error getting page: ", url)
+
+
 
 def main():
 	parser = argparse.ArgumentParser(description="Get a webpage") 
@@ -13,18 +36,21 @@ def main():
 	# Parse options from the command line [-rlp]
 	parser.add_argument("-r", "--recursive", action="store_true", help="Get the page recursively")
 	parser.add_argument("-l", "--level", type=int, help="The level of recursion")
-	parser.add_argument("-p", "--path", help="The path to save the images")
+	parser.add_argument("-p", "--path", type=str, help="The path to save the images")
 	parser.add_argument("url", help="The URL to get")
 	args = parser.parse_args()
 	url = args.url
 
-	print("URL: ", url)
-	if args.recursive:
-		print("Recursive")
-	if args.level:
-		print("Level: ", args.level)
+
 	if args.path:
-		print("Path: ", args.path)
+		path = args.path
+	else:
+		path = "images"
+	# Check if the path exists
+	if not os.path.exists(path):
+		os.makedirs(path)
+	# Get the images
+	get_images(url, path)
 
 
 main()
