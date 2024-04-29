5fb6392ad40ece17e460d1dff57e6cc56856bd2d
Yassin MEHLIL <ymehlil@bess-f4r5s4.clusters.42paris.fr>
2024-04-29 17:47:10 +0200

    end

--- arachnida/scorpion/scorpion.py
+++ arachnida/scorpion/scorpion.py
@@ -1,65 +1,36 @@
-# This program recieve imagge files as parameters and must be able to parse them for EXIF and other metadata, displaying them on the screen
-
+from exif import Image
+from colorama import Fore, Style
 import sys
-import os
-import exifread
-from PIL import Image
+from PIL import Image as PILImage  # Importer PIL pour vérifier le format
 
-
-def get_exif_data(image):
-    with open(image, 'rb') as file:
-        tags = exifread.process_file(file)
-        print('EXIF data for: ' + image)
-        for tag in tags.keys():
-            if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
-                print('Key: %s, value %s' % (tag, tags[tag]))
-        print('\n')
-
-
-def get_metadata(image):
-    img = Image.open(image)
-    print('Metadata for: ' + image)
-    print('Format: ' + img.format)
-    print('Size: ' + str(img.size))
-    print('Mode: ' + img.mode)
-    print('Info: ' + str(img.info))
-    print('Palette: ' + str(img.getpalette()))
-    print('Color: ' + str(img.getcolors()))
-    print('Histogram: ' + str(img.histogram()))
-    print('Layers: ' + str(img.getbands()))
-    print('Resolution: ' + str(img.info.get('dpi')))
-    print('ICC Profile: ' + str(img.info.get('icc_profile')))
-    print('Transparency: ' + str(img.info.get('transparency')))
-    print('Megapixels: ' + str(img.size[0] * img.size[1] / 1000000) + ' MP')
-    print('FileAccessDate: ' + str(os.path.getatime(image)))
-    print('FileModifiedDate: ' + str(os.path.getmtime(image)))
-    print('FileCreationDate: ' + str(os.path.getctime(image)))
-    print('FilePermissions: ' + str(oct(os.stat(image).st_mode)[-3:]))
-    print('FileSize: ' + str(os.path.getsize(image)) + ' bytes')
-    print('Filter: ' + str(img.filter(ImageFilter.BLUR)))
-    print("Gamma: " + str(img.info.get('gamma')))
-    print("Compression: " + str(img.info.get('compression')))
-    print('\n')
-
+extension = [".jpeg", ".jpg", ".png", ".gif", ".bmp"]
 
 def main():
-    if len(sys.argv) < 2:
-        print('Usage: python scorpion.py <image1> <image2> ... <imageN>')
-        sys.exit(1)
-
-    print('Scorpion - Image Metadata Parser\n')
-    for i in range(1, len(sys.argv)):
-        image = sys.argv[i]
-        if os.path.exists(image):
-            print('Processing: ' + image)
-            print('\n')
-            print('------------------------------------')
-            get_exif_data(image)
-            print('------------------------------------')
-            get_metadata(image)
+    # Parcours des arguments passés au script
+    for image_path in sys.argv[1:]:
+        if image_path.endswith(tuple(extension)):
+            try:
+                # Vérification du format de l'image avant de tenter de lire les EXIF
+                with open(image_path, 'rb') as image_file:
+                    pil_img = PILImage.open(image_file)
+                    format = pil_img.format
+                    print(Fore.GREEN + f"==== IMAGE : {image_path} ====")
+                    print(Fore.YELLOW + f"Image format: {format}")
+                    if format in ["JPEG", "TIFF"]:
+                        image_file.seek(0)  # Retourner au début du fichier
+                        img = Image(image_file)
+                        print(Fore.YELLOW + "EXIF data:")
+                        for tag in img.list_all():
+                            print(Fore.GREEN + f"{tag}: {img.get(tag)}")
+                    else:
+                        print(Fore.RED + "EXIF data not available for this image format.")
+                    print(Style.RESET_ALL)
+            except Exception as e:
+                print(Fore.RED + f"Error reading image {image_path}: {e}")
+                print(Style.RESET_ALL)
         else:
-            print('File not found: ' + image)
+            print(Fore.RED + f"File not found or unsupported format: {image_path}")
+            print(Style.RESET_ALL)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-        
